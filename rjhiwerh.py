158a9d72933481df65bf5267739b086ded055112
Leandro <leandro.bueno@znc.com.br>
2019-05-24 15:20:56 -0300

    melhora na raspagem, agora com Phantomjs

---
+++ rjhiwerh.py
@@ -0,0 +1,5 @@
+a = '<option value = "4397" id = "4397">PORRADA</option> ==$0'
+b = '<option value = "4398" id = "4397">PORRA</option> ==$0'
+
+print(a[34:42])
+
--- telegram.py
+++ telegram.py
@@ -1,55 +1,60 @@
 import telebot
-import time
-import re
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from urllib.request import urlopen
+import time
 from bs4 import BeautifulSoup
 
+
 def gerar_link(palavra):
+        driver = webdriver.PhantomJS()
+        driver.get("http://www.acessibilidadebrasil.org.br/libras_3/")
+        driver.implicitly_wait(10)
 
-	browser = webdriver.Firefox(executable_path="./geckodriver")
-	browser.set_window_size(100,100)
-	browser.set_window_position(0,0)
-	time.sleep(2)
-	browser.get("http://www.acessibilidadebrasil.org.br/libras_3/")
-	browser.implicitly_wait(10)
+        driver.find_element_by_xpath('//*[@id="search_field"]').click()
+        driver.find_element_by_xpath('//*[@id="search_field"]').send_keys(palavra)
+        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div[3]/div[1]/div[2]/table/tbody/tr[4]/td/input').click()
+        driver.find_element_by_xpath('//*[@id="0"]').click()
+        driver.find_element_by_xpath('//*[@id="0"]').send_keys(u'\ue015')
 
-	browser.find_element_by_xpath('//*[@id="search_field"]').click()
-	browser.find_element_by_xpath('//*[@id="search_field"]').send_keys(palavra)
-	browser.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div[3]/div[1]/div[2]/table/tbody/tr[4]/td/input').click()
-	browser.find_element_by_xpath('//*[@id="0"]').click()
-	browser.find_element_by_xpath('//*[@id="0"]').send_keys(u'\ue015')
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        time.sleep(3)
+        gif = soup.source.get('src')
+        link = "http://www.acessibilidadebrasil.org.br/libras_3/" + gif
+        return('''
+Palavra: *{palavra}*
 
-	html = browser.page_source
-	soup = BeautifulSoup(html, 'html.parser')
-	#site = "http://www.acessibilidadebrasil.org.br/libras_3/"
-	#page = urlopen(site)
-	#soup = BeautifulSoup(page, 'html.parser')
+imagem: {link}'''.format(palavra=palavra.upper(), link=link))
 
-	time.sleep(3)
-
-	imagem = soup.img.get('src')
-	link = imagem
-
-
-	return('''
-Palavra: {palavra}
-imagem: {link}'''.format(palavra=palavra, link=link))
-
-browser.quit()
 
 bot_token = '701005414:AAGUfx_l4M33Pftx_AnTllMc4_jtmICS-Xk'
 
 bot = telebot.TeleBot(token=bot_token)
 
+
 @bot.message_handler(commands=['start'])
 def msg_bv(message):
-	bot.reply_to(message, 'DICIONARIO DE LIBRAS\n Digite /help para receber ajudar!')
+	bot.reply_to(message, '''DICIONÁRIO DE LIBRAS
+
+Olá, sou o bot de Libras, comigo você tem acesso a milhares de sinais em libras de diversas palavras do português brasileiro.
+
+Espero ser muito útil e em caso de problemas entre em contato com meu pai através de seu e-mail:
+librasbot@gmail.com
+
+VAMOS COMEÇAR!!!
+
+Para isso use um dos comandos que disponibilizo.
+
+Comandos
+
+/help - para receber ajudar a qualquer momento
+/libra <palavra> - para buscar o sinal em libras da palavra deseja''')
+
 
 @bot.message_handler(commands=['help'])
 def msg_bv(message):
-	bot.reply_to(message, '"/libra <palavra>" para receber uma imagem de como é executado o sinal em libras da palavra.')
+	bot.reply_to(message, '''ESTÁ COM DIFICULDADES?! :(
+Use ''')
+
 
 @bot.message_handler(commands=['libra'])
 def comando_libra(message):
